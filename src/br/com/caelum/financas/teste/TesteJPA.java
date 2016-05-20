025cf3db4e6ed227ea86d9bc49a2faa0cf656e66
Juan Silva <juandf.silva@gmail.com>
2016-05-20 16:59:22 -0300

    .

---
+++ src/br/com/caelum/financas/teste/TesteJPA.java
@@ -0,0 +1,29 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.caelum.financas.modelo.Conta;
+
+public class TesteJPA {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		
+		conta.setTitular("Juan");
+		conta.setBanco("ITAU");
+		conta.setAgencia("1821");
+		conta.setNumero("123456");
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("financas");
+		EntityManager manager = entityManagerFactory.createEntityManager();
+		manager.getTransaction().begin();
+		manager.persist(conta);
+		manager.getTransaction().commit();
+		
+		manager.close();
+	}
+
+}
